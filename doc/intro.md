391165c2c66aade8ff7e950073b84a380c3139f6
John Jacobsen <john@mail.npxdesigns.com>
2015-10-11 13:28:43 -0500

    Cleanup.

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to rosalind
-
-TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject rosalind "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Bioinformatics problems from Rosalind, worked in Clojure."
+  :url "https://github.com/eigenhombre/rosalind"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]])
